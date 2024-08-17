8125e2ca516af1c15fdb42ba3f93da7f7d1167cf
Админ <matroskin8@i.ua>
2024-08-17 12:50:25 +0300

    lesson 2

---
+++ core/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PHPFramework;
+
+class Application
+{
+
+    protected string $uri;
+    public Request $request;
+    public static Application $app;
+
+    public function __construct()
+    {
+        self::$app = $this;
+        $this->uri = $_SERVER['QUERY_STRING'];
+        $this->request = new Request($this->uri);
+        //var_dump($this->uri);
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -6,5 +6,17 @@ if (PHP_MAJOR_VERSION < 8) {
     die('Require PHP version >= 8');
 }
 
+require_once __DIR__ . '/../config/config.php';
+require_once ROOT . '/vendor/autoload.php';
+require_once HELPERS . '/helpers.php';
 
-var_dump("Time: " . microtime(true) - $start_framework);
+$app = new \PHPFramework\Application();
+dump($app);
+dump(app());
+dump(request()->getMethod());
+dump(request()->isGet());
+dump(request()->isPost());
+dump(request()->isAjax());
+dump(request()->get('page', '10'));
+
+dump("Time: " . microtime(true) - $start_framework);
